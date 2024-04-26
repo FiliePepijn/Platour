41f6bd83fd73fc3ec0438a74e949103405b807de
Pepijn <i534787@fontysict.nl>
2024-04-26 14:21:38 +0200

    fixed a problem with the vite. made a mistake in the link from index.html to aboutMe.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite';
 import { qrcode } from 'vite-plugin-qrcode';
 
-export default defineConfig({
-    root: 'src/',
+export default {
+    
+    root: './src/',
     publicDir: '../static/',
     base: './',
-    server: {
+    server:
+    {
         host: true, // Open to local network and display URL
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
     plugins: [
         qrcode() // only applies in dev mode
     ],
-    build: {
+    build:
+    {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
-    }
-});
+    },
+}
